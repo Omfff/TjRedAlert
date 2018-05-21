3a7eed75907fb34cdb945e9dff6bf4bc6ef1da7f
Luoyu <1197182168@qq.com>
2018-05-21 14:28:21 +0800

    Commit MyUtility
    Add an func getCurrentTime

---
+++ HelloWorldDemo/Classes/MyUtility.h
@@ -0,0 +1,15 @@
+#ifndef __MY_UTILITY_H__
+#define __MY_UTILITY_H__
+
+#include"cocos2d.h"
+
+USING_NS_CC;
+
+using namespace std;
+
+class MyUtility
+{
+	static stirng getCurrentTime();
+};
+#endif // !__MY_UTILITY__
+
