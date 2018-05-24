b63c5987b3d6df16fae9a2dd6d0e4f74e3d658ab
Luoyu <1197182168@qq.com>
2018-05-24 20:57:06 +0800

    Class MouseRect
    create mouse rectangle to choose some soilders or some other things

---
+++ HelloWorldDemo/Classes/MouseRect.h
@@ -0,0 +1,28 @@
+#ifndef __MOUNSE_RECT_H__
+#define __MOUNSE_RECT_H__
+
+#include"cocos2d.h"
+
+class MouseRect :public cocos2d::DrawNode
+{
+public:
+
+	void setTouchStartToGL(cocos2d::Point point);
+	void setTouchEndToGL(cocos2d::Point point);
+	void setTouchStartToMap(cocos2d::Point mapPoint);
+	void setTouchEndToMap(cocos2d::Point mapPoint);
+
+	//update mouse renctangle by changing touchEndToMap
+	void update(float f) override;
+
+	void reset();
+
+private:
+	cocos2d::Point _touchStartToGL;
+	cocos2d::Point _touchEndToGL;
+	cocos2d::Point _touchStartToMap;
+	cocos2d::Point _touchEndToMap;
+	
+};
+#endif // !__MOUNSE_RECT_H__
+
