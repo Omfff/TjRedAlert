225713857de396aa5c9a8d0d762ac9f4c674a5eb
Luoyu <1197182168@qq.com>
2018-05-28 11:26:26 +0800

    Modify systemheader
    change #define to const int

--- HelloWorldDemo/Classes/SystemHeader.h
+++ HelloWorldDemo/Classes/SystemHeader.h
@@ -2,18 +2,18 @@
 #define __SYSTEM_HEADER_H__
 
 //Money.h
-#define INITIAL_MONEY 10000
-#define MONEY_IN_PERIOD 100
-#define PERIOD 20
+const int INITIAL_MONEY = 10000;
+const int MONEY_IN_PERIOD = 100;
+const int PERIOD = 20;
 
 
 //GamingScene.cpp --void scroll()
-#define BOX_EDGE_WIDTH_SMALL 20
-#define BOX_EDGE_WIDTH 40
-#define BOX_EDGE_HEIGHT_SMALL 20
-#define BOX_EDGE_HEIGHT 40
+const int BOX_EDGE_WIDTH_SMALL = 20;
+const int BOX_EDGE_WIDTH = 40;
+const int BOX_EDGE_HEIGHT_SMALL = 20;
+const int BOX_EDGE_HEIGHT = 40;
 
-#define SCROLL_DISTANCE 10
+const int SCROLL_DISTANCE = 10;
 
 #endif // !__SYSTEM_HEADER_H__
 
