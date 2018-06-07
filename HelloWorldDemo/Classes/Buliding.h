94eb55a69324ba1c6ec916350d373d6a39042ad7
omf2333 <1923208364@qq.com>
2018-06-08 00:24:58 +0800

    unit.h and .cpp change     building.h and .cpp change

--- HelloWorldDemo/Classes/Buliding.h
+++ HelloWorldDemo/Classes/Buliding.h
@@ -2,24 +2,44 @@
 #include"cocos2d.h"
 #include"Unit.h"
 
-//class UnitManager;
+
 class Base :public BuildingUnit
 {
 public:
-	void startProduce();
 	//CREATE_FUNC(Base);
-	void update(float f);
 	static Base* Base::create(const std::string& filename);
 	bool init( CampTypes camp, UnitTypes buildingType, GridVec2 point, TMXTiledMap* map, GridMap *gridmap, int id=0);
-	bool getDamage(int hurt);
+	//bool getDamage(int hurt);
 };
+
 class PowerPlant :public BuildingUnit
 {
 public:
-	void startProduce();
-	void update(float f);
+	//void update(float f);
 	static PowerPlant*PowerPlant::create(const std::string& filename);
-	bool init(CampTypes camp, UnitTypes buildingType, GridVec2 point, TMXTiledMap* map, GridMap *gridmap, int id = 0);
-	bool getDamage(int hurt);//建筑物的getdamage函数可以用函数模板来写
+	//bool init(CampTypes camp, UnitTypes buildingType, GridVec2 point, TMXTiledMap* map, GridMap *gridmap, int id = 0);
+	//bool getDamage(int hurt);//建筑物的getdamage函数可以用函数模板来写
 
+};
+
+class OreRefinery :public BuildingUnit
+{
+public:
+	static OreRefinery * OreRefinery::create(const std::string& filename);
+	//bool getDamage(int hurt);//建筑物的getdamage函数可以用函数模板来写
+};
+
+
+class Barracks :public BuildingUnit
+{
+public:
+	GridVec2 findEmptyPosToProduceSolider();
+	static Barracks * Barracks::create(const std::string & filename);
+};
+
+class WarFactory :public BuildingUnit
+{
+public:
+	GridVec2 findEmptyPosToProduceTank();
+	static WarFactory * WarFactory::create(const std::string &filename);
 };
