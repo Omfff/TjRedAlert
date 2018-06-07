2bd9780b80bb584431582ebdbdd0719214f29f65
omf2333 <1923208364@qq.com>
2018-06-07 09:27:53 +0800

    bullet

---
+++ HelloWorldDemo/Classes/Bullet.h
@@ -0,0 +1,33 @@
+#pragma once
+#include"UnitManager.h"
+#include"cocos2d.h"
+#include"Unit.h"
+#include"Fighter.h"
+#include"Buliding.h"
+
+
+#define _GI_BULLET_SPEED 10;
+#define _TANK_BULLET_SPEED 20;
+USING_NS_CC;
+class Unit;
+class Bullet :public Sprite
+{
+public:
+	static Bullet* create(const std::string& filename);
+	bool initWithTarget(FightUnit* sender, int target);
+	void setMap(TMXTiledMap *map);
+	void setUnitManager(UnitManager * uM);
+	//~Bullet();
+private:
+	TMXTiledMap * _tiledMap = nullptr;
+	Sprite * _bullet;
+	Unit* _target=nullptr;
+	void update(float dt);
+	int _speed;
+	int _targetId;
+	FightUnit * _sender;
+	UnitManager * _unitManager;
+
+
+};
+
